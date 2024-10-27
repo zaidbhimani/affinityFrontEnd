532446dbbc202de9e3563e54c48cd60b16045969
Zaid Bhimani <zaidbhimani786@gmail.com>
2024-10-27 19:41:19 +0530

    Prettier Formatting and Add Test for it

--- Widget task/src/components/PollWidget/PollWidget.js
+++ Widget task/src/components/PollWidget/PollWidget.js
@@ -1,52 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import styles from "./PollWidget.module.scss"
+import React, { useState, useEffect } from "react";
+import styles from "./PollWidget.module.scss";
 
 const PollWidget = (props) => {
-  const { getSelectedData = () => { }, item , selectedAnswer } = props
-  const { id, question, answers = [] } = item
+  const { getSelectedData = () => {}, item, selectedAnswer } = props;
+  const { id, question, answers = [] } = item;
 
-  const [selectedOption, setSelectedOption] = useState({})
+  const [selectedOption, setSelectedOption] = useState({});
 
   useEffect(() => {
     if (selectedAnswer !== "") {
       setSelectedOption({
         question,
-        answer: selectedAnswer
-      })
+        answer: selectedAnswer,
+      });
     }
-  }, [selectedAnswer])
-
+  }, [selectedAnswer]);
+console.log("selectedOption", selectedOption)
   const setOption = (question, element) => {
-    setSelectedOption(
-      {
-        question,
-        answer: element
-      }
-    )
+    setSelectedOption({
+      question,
+      answer: element,
+    });
     getSelectedData({
       question,
-      answer: element
-    })
-  }
+      answer: element,
+    });
+  };
 
-  return <div className={styles.pollWidgetWrapper}>
-    <div className={styles.question}>{question}</div>
-    <div className={styles.radio_group}>
-      {
-        answers.map((element, idx) => {
-          return <div key={idx} className={`${styles.poll_option} ${selectedOption?.answer === element && styles.poll_option_active}`} onClick={() => {
-            setOption(question, element)
-          }}>
-            <label className={styles.radio_button_container} disabled>
-              {element}
-              <input type="radio" checked={selectedOption?.answer === element ? "checked" : ""} name={`${question}_radio`} onChange={() => { }} />
-              <span className={styles.checkmark}></span>
-            </label>
-          </div>
-        })
-      }
-    </div>
-  </div>
-}
+  return (
+    <fieldset className={styles.pollWidgetWrapper}>
+      <legend className={styles.question}>{question}</legend>
+      <div className={styles.radio_group}>
+        {answers.map((element, idx) => {
+          return (
+            <div
+              key={idx}
+              className={`${styles.poll_option} ${
+                selectedOption?.answer === element && styles.poll_option_active
+              }`}
+              onClick={() => {
+                setOption(question, element);
+              }}
+            >
+              <label className={styles.radio_button_container}>
+                {element}
+                <input
+                  type="radio"
+                  checked={selectedOption?.answer === element}
+                  name={`${question}_radio`}
+                  onChange={() => {
+                    setOption(question, element);
+                  }}
+                />
+                <span className={styles.checkmark}></span>
+              </label>
+            </div>
+          );
+        })}
+      </div>
+    </fieldset>
+  );
+};
 
-export default PollWidget
+export default PollWidget;
---
+++ Widget task/src/components/PollWidget/PollWidget.test.js
@@ -0,0 +1,58 @@
+import { render, screen, waitFor } from "@testing-library/react";
+
+import userEvent from "@testing-library/user-event";
+
+import { questionArray } from "../../Constants";
+
+import PollWidget from "./PollWidget";
+
+describe("PollWidget", () => {
+  test("When user click on option THEN option click will be selected", async () => {
+    const getSelectedData = jest.fn();
+
+    render(
+      <PollWidget
+        item={questionArray.personal[0]}
+        selectedAnswer={""}
+        getSelectedData={getSelectedData}
+      />
+    );
+
+    expect(screen.getByLabelText(/chinese/i)).not.toBeChecked();
+
+    userEvent.click(screen.getByLabelText(/chinese/i));
+
+    expect(screen.getByLabelText(/chinese/i)).toBeChecked();
+
+    expect(getSelectedData).toHaveBeenCalledWith({
+      answer: "Chinese",
+      question: "Your favourite food?",
+    });
+  });
+
+  test("WHEN user click on option pre selected And user click on it THEN option will get de selected", async () => {
+    const getSelectedData = jest.fn();
+
+    render(
+      <PollWidget
+        item={questionArray.personal[0]}
+        selectedAnswer={"Chinese"}
+        getSelectedData={getSelectedData}
+      />
+    );
+
+    expect(screen.getByLabelText(/chinese/i)).toBeChecked();
+
+    userEvent.click(screen.getByLabelText(/biryani/i));
+
+    expect(screen.getByLabelText(/biryani/i)).toBeChecked();
+    expect(screen.getByLabelText(/chinese/i)).not.toBeChecked();
+
+    expect(getSelectedData).toHaveBeenCalledWith({
+      answer: "Biryani",
+      question: "Your favourite food?",
+    });
+
+    expect(getSelectedData).toHaveBeenCalledTimes(1);
+  });
+});
