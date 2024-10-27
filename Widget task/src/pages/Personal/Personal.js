2751982d0e9bbdee42486489a28c44128a9b08ac
Zaid Bhimani <zaidbhimani786@gmail.com>
2024-10-27 19:41:49 +0530

    Prettier FOrmatting

--- Widget task/src/pages/Personal/Personal.js
+++ Widget task/src/pages/Personal/Personal.js
@@ -1,41 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import PollWidget from '../../components/PollWidget/PollWidget';
-import AnswerDisplayer from '../../components/AnswerDisplayer/AnswerDisplayer';
-import { getPollWidgetDataFromLocalStorage } from '../../GlobalUtils';
+import React, { useState, useEffect } from "react";
+import PollWidget from "../../components/PollWidget/PollWidget";
+import AnswerDisplayer from "../../components/AnswerDisplayer/AnswerDisplayer";
+import { getPollWidgetDataFromLocalStorage } from "../../GlobalUtils";
 
 const PersonalKeyConstant = "personalPollAnswers";
 
 const setPollWidgetDataToLocalStorage = (obj) => {
   localStorage.setItem(PersonalKeyConstant, JSON.stringify(obj));
-}
+};
 
 const Personal = (props) => {
-  const { data = [] } = props
+  const { data = [] } = props;
   const [personalPollData, setPersonalPollData] = useState({});
 
   useEffect(() => {
     setTimeout(() => {
-      const localStorageObj = getPollWidgetDataFromLocalStorage(PersonalKeyConstant)
+      const localStorageObj = getPollWidgetDataFromLocalStorage(
+        PersonalKeyConstant
+      );
       if (Object.keys(localStorageObj).length > 0) {
-        setPersonalPollData(localStorageObj)
+        setPersonalPollData(localStorageObj);
       }
-    })
-  }, [])
+    });
+  }, []);
 
   const setSelectedAnswer = (selectedValue) => {
     const obj = {
       ...personalPollData,
-      [selectedValue.question]: selectedValue?.answer
-    }
-    setPersonalPollData(obj)
-    setPollWidgetDataToLocalStorage(obj)
-  }
+      [selectedValue.question]: selectedValue?.answer,
+    };
+    setPersonalPollData(obj);
+    setPollWidgetDataToLocalStorage(obj);
+  };
 
-  return <section>{data.map((element, idx) => {
-    return <PollWidget key={idx} item={element} selectedAnswer={personalPollData[element.question] ?? ""} getSelectedData={setSelectedAnswer} />
-  })}
-    {Object.keys(personalPollData).length > 0 && <AnswerDisplayer data={personalPollData} />}
-  </section>
-}
+  return (
+    <section>
+      <form>
+        {data.map((element, idx) => {
+          return (
+            <PollWidget
+              key={idx}
+              item={element}
+              selectedAnswer={personalPollData[element.question] ?? ""}
+              getSelectedData={setSelectedAnswer}
+            />
+          );
+        })}
+      </form>
+      {Object.keys(personalPollData).length > 0 && (
+        <AnswerDisplayer data={personalPollData} />
+      )}
+    </section>
+  );
+};
 
-export default Personal
+export default Personal;
